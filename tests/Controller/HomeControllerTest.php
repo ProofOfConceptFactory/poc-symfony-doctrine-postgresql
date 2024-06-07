f56296de9714c02eb28f3aa5bee470227082fa31
Abdouni Abdelkarim <abdounikarim@gmail.com>
2024-06-07 22:50:01 +0200

    Doctrine with PostgreSQL - tests

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -11,9 +11,17 @@ final class HomeControllerTest extends WebTestCase
     public function testHomepage(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'text/html; charset=UTF-8');
+        $this->assertResponseFormatSame('html');
+
         $this->assertSelectorTextContains('h1', 'Hello from Symfony!👋');
+        $this->assertEquals(1, $crawler->filter('hr')->count());
+        $this->assertEquals(3, $crawler->filter('ul li')->count());
+        $this->assertEquals('Foo', $crawler->filter('ul li')->eq(0)->text());
+        $this->assertEquals('Bar', $crawler->filter('ul li')->eq(1)->text());
+        $this->assertEquals('Baz', $crawler->filter('ul li')->eq(2)->text());
     }
 }
