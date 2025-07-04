add7181ebf54e67f5e5fe9430e97488e9ef87d1f
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-07-05 00:46:27 +0200

    About page, add design and update tests

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,13 +9,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class FrontController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'front')]
     public function home(DummyRepository $dummyRepository): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('front/index.html.twig', [
             'dummies' => $dummyRepository->findAll(),
         ]);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('front/about.html.twig');
     }
 }
--- tests/Controller/FrontControllerTest.php
+++ tests/Controller/FrontControllerTest.php
@@ -6,7 +6,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-final class HomeControllerTest extends WebTestCase
+final class FrontControllerTest extends WebTestCase
 {
     public function testHomepage(): void
     {
@@ -17,11 +17,12 @@ final class HomeControllerTest extends WebTestCase
         $this->assertResponseHeaderSame('Content-Type', 'text/html; charset=UTF-8');
         $this->assertResponseFormatSame('html');
 
+        $this->assertSelectorTextContains('title', 'Hello from Symfony, Doctrine and PostgreSQL!👋');
         $this->assertSelectorTextContains('h1', 'Hello from Symfony, Doctrine and PostgreSQL!👋');
         $this->assertEquals(1, $crawler->filter('hr')->count());
-        $this->assertEquals(3, $crawler->filter('ul li')->count());
-        $this->assertEquals('Foo', $crawler->filter('ul li')->eq(0)->text());
-        $this->assertEquals('Bar', $crawler->filter('ul li')->eq(1)->text());
-        $this->assertEquals('Baz', $crawler->filter('ul li')->eq(2)->text());
+        $this->assertEquals(3, $crawler->filter('main ul li')->count());
+        $this->assertEquals('Foo', $crawler->filter('main ul li')->eq(0)->text());
+        $this->assertEquals('Bar', $crawler->filter('main ul li')->eq(1)->text());
+        $this->assertEquals('Baz', $crawler->filter('main ul li')->eq(2)->text());
     }
 }
